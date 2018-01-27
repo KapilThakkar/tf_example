9517b046cc70ed18e296c829f1517da9edaff920
Kapil Thakkar <kapil@click2cloud.net>
2018-01-28 02:41:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,11 @@
-# Configure the Provider
-variable "user_name" {}
-variable "domain_name" {}
-variable "password" {}
-variable "tenant_name" {}
-
 terraform {
   backend "s3" {}
 }
 
-provider "opentelekomcloud" {
-  user_name   = "${var.user_name}"
-  domain_name = "${var.domain_name}"
-  password    = "${var.password}"
-  auth_url    = "https://iam.eu-de.otc.t-systems.com/v3"
-  tenant_name  ="${var.tenant_name}"
-}
-
-
-
-resource "opentelekomcloud_networking_network_v2" "selected" {
-  name = "terraform-jenkins-network"
-  admin_state_up = "true"
-}
-
-
-output "myOutput" {
-  value = "${opentelekomcloud_networking_network_v2.selected.id}"
+resource "aws_instance" "example"  {
+        ami = "ami-97785bed"
+        instance_type = "t2.micro"
+        tags {
+        Name="terraform-instance"
+        }
 }
