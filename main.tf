e1381e0cea9f3f5d0818bdd43bd67301aacf3d63
mygit676 <mydevops676@gmail.com>
2018-01-28 05:10:18 +0530

    update

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ terraform {
 }
 
 
-
 resource "aws_instance" "example"  {
         ami = "ami-97785bed"
         instance_type = "t2.micro"
@@ -11,3 +10,4 @@ resource "aws_instance" "example"  {
         Name="terraform-instance"
         }
 }
+
